44e6b6a9d3dfce9bf15adde142849b73a808e3f2
electricboogie <32370782+electricboogie@users.noreply.github.com>
2022-08-14 21:04:26 -0500

    Make everything prettier

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@
 // that was distributed with this source code.
 
 use std::{
+    collections::BTreeMap,
     error::Error,
     fmt,
-    fs::{canonicalize, File, OpenOptions},
+    fs::{canonicalize, read_dir, File, OpenOptions},
     io::{Read, Write},
     path::{Path, PathBuf},
     process::Command as ExecProcess,
@@ -33,13 +34,15 @@ fn parse_args() -> ArgMatches {
                 .value_parser(clap::builder::ValueParser::os_string())
                 .display_order(1),
         )
+        .arg(Arg::new("WRITE").short('w').long("write").display_order(2))
+        .arg(Arg::new("READ").short('r').long("read").display_order(3))
+        .arg(
+            Arg::new("WRITE_NEW")
+                .short('n')
+                .long("write-new")
+                .display_order(4),
+        )
         .get_matches()
-}
-
-#[derive(Debug, Clone)]
-pub struct Config {
-    pwd: PathBuf,
-    paths: Vec<PathBuf>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -51,8 +54,9 @@ pub struct FileInfo {
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct FileMetadata {
     hash_algo: Box<str>,
-    hash_string: Box<str>,
-    timestamp: SystemTime,
+    hash_value: u128,
+    last_checked: SystemTime,
+    modify_time: SystemTime,
 }
 
 impl FileInfo {
@@ -87,9 +91,10 @@ impl FileInfo {
                         Some((first, last)) => Ok(FileInfo {
                             path: path.to_owned(),
                             metadata: Some(FileMetadata {
-                                timestamp: timestamp.to_owned(),
+                                last_checked: timestamp.to_owned(),
                                 hash_algo: first.into(),
-                                hash_string: last.into(),
+                                hash_value: { u128::from_str_radix(last, 16)? },
+                                modify_time: path.metadata()?.modified()?,
                             }),
                         }),
                         None => Ok(FileInfo {
@@ -112,6 +117,20 @@ impl FileInfo {
 
         get_file_info(path)
     }
+}
+
+#[derive(Debug, Clone)]
+enum ExecMode {
+    ReadHashes,
+    WriteHashes,
+}
+
+#[derive(Debug, Clone)]
+pub struct Config {
+    exec_mode: ExecMode,
+    write_new: bool,
+    pwd: PathBuf,
+    paths: Vec<PathBuf>,
 }
 
 impl Config {
@@ -138,25 +157,15 @@ impl Config {
             .into());
         };
 
-        let mut paths: Vec<PathBuf> = if let Some(input_files) = matches.values_of_os("INPUT_FILES")
-        {
-            input_files
-                .par_bridge()
-                .map(Path::new)
-                .filter(|path| {
-                    if path.exists() {
-                        true
-                    } else {
-                        eprintln!("Path {:?} does not exist", path);
-                        false
-                    }
-                })
-                .flat_map(canonicalize)
-                .collect()
+        let exec_mode = if matches.is_present("READ") {
+            ExecMode::ReadHashes
         } else {
-            read_stdin()?
-                .par_iter()
-                .map(Path::new)
+            ExecMode::WriteHashes
+        };
+
+        let parse_paths = |raw_paths: Vec<&Path>| -> Vec<PathBuf> {
+            raw_paths
+                .into_par_iter()
                 .filter(|path| {
                     if path.exists() {
                         true
@@ -168,6 +177,26 @@ impl Config {
                 .flat_map(canonicalize)
                 .collect()
         };
+
+        let mut paths: Vec<PathBuf> = if let Some(input_files) = matches.values_of_os("INPUT_FILES")
+        {
+            parse_paths(input_files.par_bridge().map(Path::new).collect())
+        } else {
+            match &exec_mode {
+                ExecMode::WriteHashes => {
+                    parse_paths(read_stdin()?.par_iter().map(Path::new).collect())
+                }
+                ExecMode::ReadHashes => read_dir(&pwd)?
+                    .par_bridge()
+                    .flatten()
+                    .map(|dir_entry| dir_entry.path())
+                    .collect(),
+            }
+        };
+
+        if paths.is_empty() {
+            return Err(DanoError::new("No valid paths to search.").into());
+        }
 
         // deduplicate path_buf and sort --
         // so input of ./.z* and ./.zshrc will only print ./.zshrc once
@@ -181,7 +210,14 @@ impl Config {
             paths
         };
 
-        Ok(Config { pwd, paths })
+        let write_new = matches.is_present("WRITE_NEW");
+
+        Ok(Config {
+            exec_mode,
+            write_new,
+            pwd,
+            paths,
+        })
     }
 }
 
@@ -198,32 +234,146 @@ fn main() {
 fn exec() -> DanoResult<()> {
     let config = Config::new()?;
 
-    let hashes = get_hashes(&config)?;
+    let mut input_file = read_input_file(&config.pwd)?;
+    let mut buffer = String::new();
+    input_file.read_to_string(&mut buffer)?;
 
+    let hashes_from_file: Vec<FileInfo> = buffer.lines().flat_map(deserialize).collect();
+    let hashes_from_paths = hashes_from_paths(&config.paths)?;
+
+    match &config.exec_mode {
+        ExecMode::WriteHashes => {
+            let (new_files, _) = partition_new_old_files(&hashes_from_file, &hashes_from_paths);
+
+            write_new_files(&config, &new_files)
+        }
+        ExecMode::ReadHashes => {
+            let new_files = compare_hash_collections(&hashes_from_file, &hashes_from_paths)?;
+
+            if !new_files.is_empty() && config.write_new {
+                write_new_files(&config, &new_files)?;
+            }
+
+            Ok(())
+        }
+    }
+}
+
+fn write_new_files(config: &Config, new_files: &[FileInfo]) -> DanoResult<()> {
+    let mut output_file = write_output_file(&config.pwd)?;
+
+    new_files.iter().try_for_each(|file_info| {
+        eprintln!("Writing new file: {:?}", file_info.path);
+        let serialized = serialize(file_info)?;
+        let out_string = serialized + "\n";
+        write_out(&out_string, &mut output_file)?;
+        Ok(())
+    })
+}
+
+fn partition_new_old_files(
+    hashes_from_file: &[FileInfo],
+    hashes_from_paths: &[FileInfo],
+) -> (Vec<FileInfo>, Vec<FileInfo>) {
+    let hashes_from_file_map: BTreeMap<PathBuf, Option<FileMetadata>> = hashes_from_file
+        .iter()
+        .cloned()
+        .map(|file_info| (file_info.path, file_info.metadata))
+        .collect::<BTreeMap<PathBuf, Option<FileMetadata>>>();
+
+    hashes_from_paths
+        .iter()
+        .cloned()
+        .partition(|file_info| !hashes_from_file_map.contains_key(&file_info.path))
+}
+
+fn compare_hash_collections(
+    hashes_from_file: &[FileInfo],
+    hashes_from_paths: &[FileInfo],
+) -> DanoResult<Vec<FileInfo>> {
+    let hashes_from_file_map: BTreeMap<PathBuf, Option<FileMetadata>> = hashes_from_file
+        .iter()
+        .cloned()
+        .map(|file_info| (file_info.path, file_info.metadata))
+        .collect::<BTreeMap<PathBuf, Option<FileMetadata>>>();
+
+    let (new_files, _): (Vec<FileInfo>, Vec<FileInfo>) =
+        partition_new_old_files(hashes_from_file, hashes_from_paths);
+
+    let phantom_files: Vec<FileInfo> = hashes_from_file
+        .iter()
+        .chain(hashes_from_paths.iter())
+        .filter(|file_info| file_info.metadata.is_none())
+        .cloned()
+        .collect();
+
+    let (modified_files, suspicious_modified_files): (Vec<FileInfo>, Vec<FileInfo>) =
+        hashes_from_paths
+            .iter()
+            .filter(|file_info| file_info.metadata.is_some())
+            .filter(|file_info| hashes_from_file_map.get(&file_info.path).is_some())
+            .filter(|file_info| hashes_from_file_map.get(&file_info.path).unwrap().is_some())
+            .cloned()
+            // known okay to unwrap because we filter on the two conditions above
+            .partition(|file_info| {
+                let map_entry = hashes_from_file_map
+                    .get(&file_info.path)
+                    .as_ref()
+                    .unwrap()
+                    .as_ref()
+                    .unwrap();
+                map_entry.modify_time == file_info.to_owned().metadata.unwrap().modify_time
+            });
+
+    eprintln!("New files:");
+    new_files
+        .iter()
+        .for_each(|file_info| eprintln!("\t{:?}", file_info.path));
+
+    eprintln!("Phantom files:\n{:?}", phantom_files);
+    phantom_files
+        .iter()
+        .for_each(|file_info| eprintln!("\t{:?}", file_info.path));
+
+    eprintln!("Modified files:\n{:?}", modified_files);
+    modified_files
+        .iter()
+        .for_each(|file_info| eprintln!("\t{:?}", file_info.path));
+
+    eprintln!("Suspicious files:\n{:?}", suspicious_modified_files);
+    suspicious_modified_files
+        .iter()
+        .for_each(|file_info| eprintln!("\t{:?}", file_info.path));
+
+    Ok(new_files)
+}
+
+fn read_input_file(pwd: &Path) -> DanoResult<File> {
+    if let Ok(input_file) = OpenOptions::new()
+        .read(true)
+        .open(pwd.join("dano_hashes.txt"))
+    {
+        Ok(input_file)
+    } else {
+        Err(DanoError::new("dano could not open a file to write to...").into())
+    }
+}
+
+fn write_output_file(pwd: &Path) -> DanoResult<File> {
     // creates script file in user's home dir or will fail if file already exists
-    let mut dano_file = if let Ok(dano_file) = OpenOptions::new()
+    if let Ok(output_file) = OpenOptions::new()
         // should overwrite the file always
         // FYI append() is for adding to the file
         .append(true)
         // create_new() will only create if DNE
         // create on a file that exists just opens
         .create(true)
-        .open(config.pwd.join("dano_hashes.txt"))
+        .open(pwd.join("dano_hashes.txt"))
     {
-        dano_file
+        Ok(output_file)
     } else {
-        return Err(DanoError::new("dano could not open a file to write to...").into());
-    };
-
-    let res: DanoResult<()> = hashes
-        .into_iter().try_for_each(|file_info| {
-            let serialized = serialize_to_json(&file_info)?;
-            let out_string = serialized + "\n";
-            write_out(&out_string, &mut dano_file)?;
-            Ok(())
-        });
-
-    res
+        Err(DanoError::new("dano could not open a file to write to...").into())
+    }
 }
 
 fn write_out(out_string: &str, open_file: &mut File) -> DanoResult<()> {
@@ -232,14 +382,16 @@ fn write_out(out_string: &str, open_file: &mut File) -> DanoResult<()> {
         .map_err(|err| err.into())
 }
 
-fn serialize_to_json(file_info: &FileInfo) -> DanoResult<String> {
+fn serialize(file_info: &FileInfo) -> DanoResult<String> {
     serde_json::to_string(&file_info).map_err(|err| err.into())
 }
 
-fn get_hashes(config: &Config) -> DanoResult<Vec<FileInfo>> {
-    let mut hashes: Vec<FileInfo> = config
-        .paths
-        .clone()
+fn deserialize(line: &str) -> DanoResult<FileInfo> {
+    serde_json::from_str(line).map_err(|err| err.into())
+}
+
+fn hashes_from_paths(paths: &[PathBuf]) -> DanoResult<Vec<FileInfo>> {
+    let mut hashes: Vec<FileInfo> = paths
         .par_iter()
         .flat_map(|path| FileInfo::new(path.as_path()))
         .collect();
