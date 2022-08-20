7d69bfb5fdfd042f96b3ec3932229951cceb04da
electricboogie <32370782+electricboogie@users.noreply.github.com>
2022-08-20 09:01:35 -0500

    Print clearer messages on new writes

--- src/process_file_info.rs
+++ src/process_file_info.rs
@@ -25,7 +25,8 @@ use crate::{Config, DanoResult, ExecMode, XattrMode};
 
 use crate::lookup_file_info::{FileInfo, FileMetadata};
 use crate::util::{
-    deserialize, print_file_info, print_out_buf, read_input_file, write_all_new_paths, WriteType,
+    deserialize, print_err_buf, print_file_info, print_out_buf, read_input_file,
+    write_all_new_paths, WriteType,
 };
 
 #[derive(Debug, Clone)]
@@ -85,72 +86,115 @@ pub fn exec_process_file_info(
 
 pub fn write_new_file_info(config: &Config, new_files_bundle: &NewFilesBundle) -> DanoResult<()> {
     // write new files - no hash match in record
-    if !new_files_bundle.new_files.is_empty()
-        && (matches!(config.exec_mode, ExecMode::Write(_))
-            || (matches!(config.exec_mode, ExecMode::Compare) && config.opt_write_new))
-    {
-        write_all_new_paths(config, &new_files_bundle.new_files, WriteType::Append)?
-    } else if !config.opt_silent && matches!(config.exec_mode, ExecMode::Write(_)) {
-        eprintln!("No new paths to write.");
+    if !new_files_bundle.new_files.is_empty() {
+        match config.exec_mode {
+            ExecMode::Write(_) | ExecMode::Compare if config.opt_write_new => {
+                new_files_bundle
+                    .new_files
+                    .iter()
+                    .try_for_each(|file_info| {
+                        print_err_buf(&format!("Writing dano hash for: {:?}\n", file_info.path))
+                    })?;
+                write_all_new_paths(config, &new_files_bundle.new_files, WriteType::Append)?
+            }
+            ExecMode::Compare => new_files_bundle
+                .new_files
+                .iter()
+                .try_for_each(|file_info| {
+                    print_err_buf(&format!(
+                        "Not writing dano hash for: {:?}, --write-new was not specified.\n",
+                        file_info.path
+                    ))
+                })?,
+            _ => unreachable!(),
+        }
+    } else {
+        eprintln!("No new file paths to write.");
     }
 
     // write old files with new names - hash matches
-    if !new_files_bundle.new_filenames.is_empty()
-        && ((matches!(config.exec_mode, ExecMode::Write(_)) && config.opt_overwrite_old)
-            || (config.exec_mode == ExecMode::Compare
-                && config.opt_overwrite_old
-                && config.opt_write_new))
-    {
-        // append new paths
-        write_all_new_paths(config, &new_files_bundle.new_filenames, WriteType::Append)?;
-
-        // overwrite all paths if in non-xattr/file write mode
-        match &config.exec_mode {
-            ExecMode::Write(write_config)
-                if matches!(write_config.opt_xattr, XattrMode::Disabled) =>
+    if !new_files_bundle.new_filenames.is_empty() && config.opt_overwrite_old {
+        match config.exec_mode {
+            ExecMode::Write(_) | ExecMode::Compare
+                if config.opt_overwrite_old && config.opt_write_new =>
             {
-                // read back
-                let recorded_file_info_with_duplicates: Vec<FileInfo> =
-                    if config.output_file.exists() {
-                        let mut input_file = read_input_file(config)?;
-                        let mut buffer = String::new();
-                        input_file.read_to_string(&mut buffer)?;
-                        // important this blows up because if you change the struct it can't deserialize
-                        buffer
-                            .lines()
-                            .filter(|line| !line.starts_with("//"))
-                            .map(deserialize)
-                            .collect::<DanoResult<Vec<FileInfo>>>()?
-                    } else {
-                        Vec::new()
-                    };
-
-                // then dedup
-                let unique_paths: Vec<FileInfo> = recorded_file_info_with_duplicates
+                new_files_bundle
+                    .new_filenames
                     .iter()
-                    .into_group_map_by(|file_info| match &file_info.metadata {
-                        Some(metadata) => metadata.hash_value,
-                        None => u128::MIN,
-                    })
-                    .into_iter()
-                    .flat_map(|(_hash, group_file_info)| {
-                        group_file_info.into_iter().max_by_key(|file_info| {
-                            match &file_info.metadata {
-                                Some(metadata) => metadata.last_written,
-                                None => SystemTime::UNIX_EPOCH,
-                            }
-                        })
-                    })
-                    .cloned()
-                    .collect();
-
-                // and overwrite
-                write_all_new_paths(config, &unique_paths, WriteType::OverwriteAll)
+                    .try_for_each(|file_info| {
+                        print_err_buf(&format!("Overwriting dano hash for: {:?}\n", file_info.path))
+                    })?;
+                overwrite_old(config, new_files_bundle)?
             }
-            _ => Ok(()),
+            ExecMode::Compare => {
+                new_files_bundle
+                    .new_filenames
+                    .iter()
+                    .try_for_each(|file_info| {
+                        print_err_buf(&format!(
+                            "Not overwriting dano hash for: {:?}, --overwrite is not specified.\n",
+                            file_info.path
+                        ))
+                    })?
+            }
+            _ => unreachable!(),
         }
     } else {
-        Ok(())
+        if config.opt_overwrite_old {
+            eprintln!("No old file paths to overwrite.");
+        } else {
+            eprintln!("File overwrite was not specified.");
+        }
+    }
+
+    Ok(())
+}
+
+fn overwrite_old(config: &Config, new_files_bundle: &NewFilesBundle) -> DanoResult<()> {
+    // append new paths
+    write_all_new_paths(config, &new_files_bundle.new_filenames, WriteType::Append)?;
+
+    // overwrite all paths if in non-xattr/file write mode
+    match &config.exec_mode {
+        ExecMode::Write(write_config) if matches!(write_config.opt_xattr, XattrMode::Disabled) => {
+            // read back
+            let recorded_file_info_with_duplicates: Vec<FileInfo> = if config.output_file.exists() {
+                let mut input_file = read_input_file(config)?;
+                let mut buffer = String::new();
+                input_file.read_to_string(&mut buffer)?;
+                // important this blows up because if you change the struct it can't deserialize
+                buffer
+                    .lines()
+                    .filter(|line| !line.starts_with("//"))
+                    .map(deserialize)
+                    .collect::<DanoResult<Vec<FileInfo>>>()?
+            } else {
+                Vec::new()
+            };
+
+            // then dedup
+            let unique_paths: Vec<FileInfo> = recorded_file_info_with_duplicates
+                .iter()
+                .into_group_map_by(|file_info| match &file_info.metadata {
+                    Some(metadata) => metadata.hash_value,
+                    None => u128::MIN,
+                })
+                .into_iter()
+                .flat_map(|(_hash, group_file_info)| {
+                    group_file_info
+                        .into_iter()
+                        .max_by_key(|file_info| match &file_info.metadata {
+                            Some(metadata) => metadata.last_written,
+                            None => SystemTime::UNIX_EPOCH,
+                        })
+                })
+                .cloned()
+                .collect();
+
+            // and overwrite
+            write_all_new_paths(config, &unique_paths, WriteType::OverwriteAll)
+        }
+        _ => Ok(()),
     }
 }
 
