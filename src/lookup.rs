9cf8fb815152322956b293c51bd770d3b3f3ba15
electricboogie <32370782+kimono-koans@users.noreply.github.com>
2023-05-27 13:57:11 -0500

    Revert "Use installed  threadpool par iter instead of scoped threads"
    This reverts commit 0e19f8cfc773e612a7ffceb2826e308b83f0e39c.

--- src/lookup.rs
+++ src/lookup.rs
@@ -25,7 +25,7 @@ use std::{
 };
 
 use crossbeam::channel::{Receiver, Sender};
-use rayon::{prelude::*, ThreadPool};
+use rayon::ThreadPool;
 use serde::{Deserialize, Serialize};
 use which::which;
 
@@ -259,17 +259,19 @@ impl FileInfoLookup {
 
         let config_arc = Arc::new(config.clone());
 
-        let _handle = std::thread::spawn(move || {
+        std::thread::spawn(move || {
             // exec threads to hash files
-            thread_pool.install(|| {
-                requested_paths_clone.par_iter().for_each(|request| {
+            thread_pool.in_place_scope(|file_info_scope| {
+                requested_paths_clone.iter().for_each(|request| {
                     let tx_item_clone = tx_item.clone();
                     let config_clone = config_arc.clone();
-                    if let Err(err) = FileInfo::generate(config_clone, request, tx_item_clone) {
-                        // probably want to see the error, but not exit the process
-                        // when there is an error in a single thread
-                        eprintln!("Error: {}", err);
-                    }
+                    file_info_scope.spawn(move |_| {
+                        if let Err(err) = FileInfo::generate(config_clone, request, tx_item_clone) {
+                            // probably want to see the error, but not exit the process
+                            // when there is an error in a single thread
+                            eprintln!("Error: {}", err);
+                        }
+                    })
                 });
             });
         });
